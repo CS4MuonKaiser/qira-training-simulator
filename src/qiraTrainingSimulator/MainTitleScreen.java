13f85c3996291f57cd885c4ce76870188a8b225b
reesemulsid <96434391+reesemulsid@users.noreply.github.com>
2023-03-20 08:54:40 +0800

    Delete MainTitleScreen.java

--- src/qiraTrainingSimulator/MainTitleScreen.java
+++
@@ -1,9 +0,0 @@
-package qiraTrainingSimulator;
-
-import javax.swing.*;
-
-public class MainTitleScreen {
-    public static void main(String[] args) {
-        JFrame window = new MainTitleScreenLayout();
-    }
-}
