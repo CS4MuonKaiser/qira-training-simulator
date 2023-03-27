756c4e338df49b477e747713244b1c1a19ea84dd
reesemulsid <96434391+reesemulsid@users.noreply.github.com>
2023-03-27 19:33:05 +0800

    Delete BattleScreen.java

--- src/qiraTrainingSimulator/BattleScreen.java
+++
@@ -1,9 +0,0 @@
-package qiraTrainingSimulator;
-
-import javax.swing.*;
-
-public class BattleScreen {
-    public static void main(String[] args) {
-        JFrame window = new BattleLayout();
-    }
-}
