b6f048fbbfd64648caca0082f9a41f720dfdc961
reesemulsid <96434391+reesemulsid@users.noreply.github.com>
2023-03-31 11:02:15 +0800

    Add files via upload

---
+++ src/qiraTrainingSimulator/TitleScreen.java
@@ -0,0 +1,9 @@
+package qiraTrainingSimulator;
+
+import javax.swing.*;
+
+public class TitleScreen {
+    public static void main(String[] args) {
+        JFrame window = new TitleLayout();
+    }
+}
