92ab9092e78841cfd23c2787884b34b421ad0212
reesemulsid <96434391+reesemulsid@users.noreply.github.com>
2023-05-10 16:40:24 +0800

    Delete src/qiraTrainingSimulator directory

--- src/qiraTrainingSimulator/Layouts/TitleScreen.java
+++
@@ -1,9 +0,0 @@
-package qiraTrainingSimulator.Layouts;
-
-import javax.swing.*;
-
-public class TitleScreen {
-    public static void main(String[] args) {
-        JFrame window = new TitleLayout();
-    }
-}
